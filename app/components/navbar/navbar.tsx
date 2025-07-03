e27c1788bc6bf20376d1a5dcc6a56bb56790dfbb
IndoPhoenix <vigyanavart@gmail.com>
2025-07-03 12:07:28 +0530

    minor changes

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,6 +1,33 @@
 "use client"
-import { AnimationPlaybackControlsWithThen, motion, stagger, useAnimate} from "motion/react"
+import Link from "next/link";
+import { AnimatePresence, AnimationPlaybackControlsWithThen, motion, stagger, useAnimate} from "motion/react"
 import { useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
+
+
+
+const content = [
+    {
+        title: "Home",
+        link: "/"
+    },
+    {
+        title: "Projects",
+        link: "/projects"
+    },
+    {
+        title: "About",
+        link: "/about"
+    },
+    {
+        title: "Blogs",
+        link: "/blog"
+    },
+    {
+        title: "Contact",
+        link: "/contact"
+    }
+]
 
 export default function Nav({width,height,strokeWidth}:{width:string,height:string,strokeWidth:string}){
 
@@ -16,8 +43,35 @@ export default function Nav({width,height,strokeWidth}:{width:string,height:stri
 function NavIcon({width,height,strokeWidth}:{width:string, height:string,strokeWidth:string}){
 
     const [scope, animate] = useAnimate();
+    const [open, setOpen] = useState();
     const [active,setActive] = useState(false);
     const animationRef = useRef<AnimationPlaybackControlsWithThen|null>(null);
+    const path = usePathname();
+    console.log(path);
+
+    const childVariant = {
+        open: {
+            opacity: 100,
+            x:0,
+            scale: 1,
+        },
+        closed: {
+            opacity: 0,
+            x: 10,
+            scale: 0.5,
+        }
+    }
+    const parentVariant = {
+        open: {
+            transition: { staggerChildren: 0.1, delayChildren: 0.2}
+        },
+        closed: {
+            width: 0,
+
+        }
+    }
+
+
     function handleActive(){
       const x =   animate(
             ".dot",
@@ -32,10 +86,48 @@ function NavIcon({width,height,strokeWidth}:{width:string, height:string,strokeW
         else animationRef.current?.cancel();
     },[active])
 
-    return <motion.div ref={scope} className="group flex flex-col gap-2" style={{width, height}} onMouseEnter={()=>setActive(true)} onMouseLeave={()=>setActive(false)}>
-  <motion.div className={`rounded-full w-full bg-white h-[${strokeWidth }] dot shadow-cardShadow`} ></motion.div>
-  <motion.div className={`rounded-full w-full bg-white h-[${strokeWidth}] dot shadow-cardShadow`}></motion.div>
-  <motion.div className={`rounded-full w-full bg-white h-[${strokeWidth}] dot shadow-cardShadow`}></motion.div>
-</motion.div>
+    return (
+        
+        <motion.div ref={scope} className="flex gap-5" 
+        animate={open?"open":"closed"}
+        transition={{duration: 1.5, ease: "easeInOut"}}
 
+        >
+            <motion.div className="" >
+                <AnimatePresence mode="wait">
+                { open && <motion.div key={"nav"} className="flex flex-col gap-4"
+                variants={parentVariant}
+                exit={"closed"}
+                // transition={{duration: 1.5}}
+                >
+                    {
+                        content.map((tab,idx)=>{
+                            return <motion.div style={{y: -5, x:10, opacity: 0 }} className="relative tabs flex gap-2 items-center justify-start" key={idx}
+                            variants={childVariant}
+                            whileHover={{scale: 1.1}}
+                            transition={{duration: 0.3}}
+                          
+                            
+                            
+                            >
+                                 <motion.span className={`h-[5px] w-[5px] bg-white rounded-full absolute -left-3 ${!(path==tab.link) && "hidden"}`}
+                                 initial={{opacity:0, y:-1000}}
+                                 animate={{opacity:1, y:0}}
+                                 transition={{type:"spring", stiffness: 300, damping: 20}}
+                                 ></motion.span>   <Link href={tab.link}>{tab.title}</Link>
+                            </motion.div>
+                        })
+                    }
+                </motion.div>}
+                </AnimatePresence>
+            </motion.div>
+            
+            <motion.div  className="group flex flex-col gap-2 active:scale-[0.95] transition-scale duration-300" style={{width, height}} onMouseEnter={()=>setActive(true)} onMouseLeave={()=>setActive(false)} onClick={()=>{setOpen(e=>!e)}}>
+  <motion.div className={`rounded-full w-full bg-white  dot shadow-cardShadow`} style={{height:strokeWidth }}></motion.div>
+  <motion.div className={`rounded-full w-full bg-white  dot shadow-cardShadow`} style={{height:strokeWidth }}></motion.div>
+  <motion.div className={`rounded-full w-full bg-white  dot shadow-cardShadow`} style={{height:strokeWidth }}></motion.div>
+</motion.div>
+        </motion.div>
+  
+    )
 }
---
+++ app/fake.tsx
@@ -0,0 +1,104 @@
+"use client"
+import Image from "next/image";
+import {useAnimate, motion, stagger} from "motion/react"
+import profile from "./assets/protfolio-image.png"
+import React, { useEffect, useRef, useState } from "react";
+import Navbar from "./components/navbar/navbar"
+import {IconBrandGithub, IconBrandLinkedin, IconBrandLinkedinFilled, IconBrandTwitter, IconX} from "@tabler/icons-react"
+import linkedIn from "./assets/linkedIn.svg"
+
+
+export default function Home() {
+
+  const [scope,animate]  = useAnimate();
+  const [mousePosition, setMousePosition] = useState<{x:number,y:number}>({x:0, y:0});
+  function FadingName(){
+    animate(".cuties",
+      {opacity: [0,1], filter: ["blur(10px)", "blur(0px)"]},
+      {delay: stagger(0.05)})
+  }
+
+ function getRelativeCoordinates(event:React.MouseEvent, referenceElement: HTMLSpanElement) {
+  console.log(scope.current)
+   const rect = {
+    left: referenceElement.offsetLeft,
+    top: referenceElement.offsetTop
+   }
+  console.log(rect.left,rect.top);
+  
+  const res =  {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+  console.log(res);
+  return res;
+}
+  
+const handleMouseMove = (e:React.MouseEvent) => {
+    setMousePosition(getRelativeCoordinates(e, scope.current));
+  };
+
+  useEffect(()=>{
+    FadingName()
+  },[])
+  return (
+  
+    <div ref={scope} className="min-h-screen w-[100vw] bg-neutral-950 flex justify-center items-center pointer " onMouseMove={(e)=>{
+      handleMouseMove(e)
+    }}>
+      <motion.span className="w-4 h-4 bg-white rounded-full absolute left-0 top-0 z-49 " style={{pointerEvents: "none"}} 
+      animate={{x: mousePosition.x -12 , y:mousePosition.y -12}}
+      transition={{type: "tween"}} >
+        <motion.span className="w-2 h-2 rounded-full bg-black absolute inset-0 m-auto z-50"
+      
+        ></motion.span>
+      </motion.span>
+      <div className="absolute right-0 top-0 m-10">
+     <Navbar width={"3rem"} height="2.5rem" strokeWidth="5px"></Navbar>
+      </div>
+   
+    <div className="flex flex-col h-full flex-1 gap-6 items-center ">
+      <div className="flex items-center flex-col relative" >
+        <Image className="rounded-full hover:scale-[1.02] active:scale-[0.98] transition-scale duration-300 " width={400} src={profile} alt="profile" onClick={FadingName}></Image>
+      <div className=" text-8xl flex gap-6 absolute bottom-2 " >
+      <SplitedText word={"Ramanshu"} className="m-1"></SplitedText>
+      <SplitedText word={"Sharan"} className="m-1"></SplitedText>
+      <SplitedText word={"Mishra"} className="m-1"></SplitedText>
+      </div>
+      </div>
+       
+      <div className="flex items-center justify-center text-xl font-bold italic"> I am someone who is trying to build something</div>
+    </div>  
+
+    <div className="absolute flex   justify-around items-center w-fit bottom-6 right-10 gap-8">
+      <IconBrandGithub className="  hover:scale-[1.3] active:scale-[0.98] transition-all duration-300"></IconBrandGithub>
+      <IconBrandTwitter className=  "hover:scale-[1.3] active:scale-[0.98] transition-all duration-300"></IconBrandTwitter>
+      <Image src={linkedIn} className="text-neutral-50 hover:scale-[1.3] active:scale-[0.98] transition-all duration-300" alt="linkedIn"></Image>   
+      <div className="font-bold hover:scale-[1.2] active:scale-[0.98] transition-all duration-300" >Resume</div>
+    </div>
+     
+   </div>
+   
+  );
+}
+
+
+function SplitedText({word, className}:{word:string, className?: string}){
+    return(
+      <div className="flex">
+      {word.split("").map((char,idx)=>{
+        return <motion.div 
+        style={{fontWeight: 400}}
+          key={idx+char} 
+          className={className+ " cuties"}
+          initial={{ scale: 1 }}
+          whileHover={{ scaleY: 1.2, scaleX: 1.3, fontWeight: "700", letterSpacing: "0.1em" }}
+          animate={{ scale: 1 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        >
+          {char}
+        </motion.div>
+      })}
+      </div>
+    )
+}
