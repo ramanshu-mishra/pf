264889d586985a969e5dc59993939c33d784c724
IndoPhoenix <vigyanavart@gmail.com>
2025-07-05 13:21:37 +0530

    minor changes

--- app/components/outerContent/outerContent.tsx
+++ app/components/outerContent/outerContent.tsx
@@ -7,7 +7,7 @@ import Image from "next/image"
 export default function Page({children}:{children:React.ReactNode}) {
     return (
         <div className="min-h-screen w-full relative flex flex-col justify-between items-center overflow-y-auto">
-            <div className="w-full mt-6 flex flex-col sm:flex-row justify-between items-center px-4 z-[950] gap-4">
+            <div className="w-full mt-6 flex flex-row justify-between items-center px-4 z-[950] gap-4">
                 <div className="text-2xl flex gap-2 items-center">
                     <SplitedText word={"Ramanshu"} className="" />
                     <SplitedText word={"Sharan"} className="hidden sm:inline" />
@@ -20,14 +20,14 @@ export default function Page({children}:{children:React.ReactNode}) {
                 {children}
             </main>
 
-            <footer className="w-full flex flex-col sm:flex-row justify-between items-end px-4 pb-4 z-[950] gap-4">
+            <footer className="w-full flex flex-row justify-start sm:justify-between items-end px-4 pb-4 z-[950] gap-4">
                 <div className="text-2xl flex gap-2">
                     <SplitedText word={"Also"} className="" />
                     <SplitedText word={"I'm"} className="" />
                     <SplitedText word={"Iron"} className="" />
                     <SplitedText word={"Man"} className="" />
                 </div>
-                <div className="flex justify-around items-center gap-6 mt-4 sm:mt-0">
+                <div className="hidden  sm:flex justify-around items-center gap-6 mt-4 sm:mt-0">
                     <IconBrandGithub className="hover:scale-[1.3] active:scale-[0.98] transition-all duration-300" />
                     <IconBrandTwitter className="hover:scale-[1.3] active:scale-[0.98] transition-all duration-300" />
                     <Image src={linkedIn} className="text-neutral-50 hover:scale-[1.3] active:scale-[0.98] transition-all duration-300" alt="linkedIn" />
